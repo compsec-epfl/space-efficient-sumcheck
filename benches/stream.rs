ea13130ad00b1d018237785161e5f77f8af083bd
Andrew Z <zitek936@gmail.com>
2023-12-17 20:56:56 +0100

    group benches by algorithm

--- benches/stream.rs
+++ benches/stream.rs
@@ -1,5 +1,5 @@
-use ark_ff::Field;
-use ark_test_curves::bls12_381::Fr as BenchField;
+use ark_ff::{Field, fields::{Fp64, Fp128, MontBackend, MontConfig}};
+use ark_bn254::Fr as BN254Field;
 
 use space_efficient_sumcheck::{
     provers::{
@@ -10,10 +10,22 @@ use space_efficient_sumcheck::{
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use jemalloc_ctl::{epoch, stats};
-use std::{thread, time::Duration};
+use std::time::Instant;
 
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[derive(MontConfig)]
+#[modulus = "18446744069414584321"] // q = 2^64 - 2^32 + 1
+#[generator = "2"]
+pub struct FieldConfig64;
+pub type Field64 = Fp64<MontBackend<FieldConfig64, 1>>;
+
+#[derive(MontConfig)]
+#[modulus = "143244528689204659050391023439224324689"] // q = 143244528689204659050391023439224324689 
+#[generator = "2"]
+pub struct FieldConfig128;
+pub type Field128 = Fp128<MontBackend<FieldConfig128, 2>>;
 
 // BenchEvaluationStream just returns the field value of the index and uses constant memory
 pub struct BenchEvaluationStream<F: Field> {
@@ -63,72 +75,129 @@ impl<F: Field> EvaluationStream<F> for BenchEvaluationStream<F> {
     }
 }
 
-fn run_bench<F: Field, P: Prover<F>>(c: &mut Criterion, mut prover: P, label: String) {
+fn run_bench<F: Field, P: Prover<F>>(_c: &mut Criterion, mut prover: P, label: String) {
     let mut rng = ark_std::test_rng();
     epoch::advance().unwrap();
-    c.bench_function(&label, |b: &mut criterion::Bencher<'_>| {
-        b.iter(|| {
-            Sumcheck::prove(&mut prover, &mut rng);
-        });
-    });
+    let start_time = Instant::now();
+    Sumcheck::prove(&mut prover, &mut rng);
+    // Record the end time
+    let end_time = Instant::now();
+
+    // Calculate the elapsed time
+    let elapsed_time = end_time - start_time;
+
     let allocated = stats::allocated::read().unwrap();
     let resident = stats::resident::read().unwrap();
-    println!("{} bytes allocated/{} bytes resident", allocated, resident);
-    thread::sleep(Duration::from_secs(10));
+    println!("{}, {:?}, {}, {}", label, elapsed_time.as_secs_f64(), allocated, resident);
 }
 
-fn all_benches(c: &mut Criterion) {
-    let max = 16;
-    // vsbw
+fn vsbw_benches(c: &mut Criterion) {
+    let max = 30;
+    // 64 bit field
     for num_variables in 15..=max {
-        let stream: BenchEvaluationStream<BenchField> = BenchEvaluationStream::new(num_variables);
-        let prover = TimeProver::<BenchField>::new(Box::new(&stream));
+        let stream: BenchEvaluationStream<Field64> = BenchEvaluationStream::new(num_variables);
+        let prover = TimeProver::<Field64>::new(Box::new(&stream));
         run_bench(
             c,
             prover,
-            String::from("vsbw") + &format!("-{}", num_variables),
+            String::from("vsbw-fp64") + &format!("-{}", num_variables),
         );
     }
-    // tradeoff k=2
+    // 128 bit field
     for num_variables in 15..=max {
-        let stream: BenchEvaluationStream<BenchField> = BenchEvaluationStream::new(num_variables);
-        let prover = TradeoffProver::<BenchField>::new(Box::new(&stream), 2);
-        if num_variables % 2 == 0 {
-            run_bench(
-                c,
-                prover,
-                String::from("tradoff2") + &format!("-{}", num_variables),
-            );
-        }
+        let stream: BenchEvaluationStream<Field128> = BenchEvaluationStream::new(num_variables);
+        let prover = TimeProver::<Field128>::new(Box::new(&stream));
+        run_bench(
+            c,
+            prover,
+            String::from("vsbw-fp128") + &format!("-{}", num_variables),
+        );
     }
-    // tradeoff k=3
+    // bn254
     for num_variables in 15..=max {
-        let stream: BenchEvaluationStream<BenchField> = BenchEvaluationStream::new(num_variables);
-        let prover = TradeoffProver::<BenchField>::new(Box::new(&stream), 3);
-        if num_variables % 3 == 0 {
-            run_bench(
-                c,
-                prover,
-                String::from("tradeoff3") + &format!("-{}", num_variables),
-            );
-        }
-        // cty
-        for num_variables in 15..=max {
-            let stream: BenchEvaluationStream<BenchField> =
-                BenchEvaluationStream::new(num_variables);
-            let prover = SpaceProver::<BenchField>::new(Box::new(&stream));
-            run_bench(
-                c,
-                prover,
-                String::from("cty") + &format!("-{}", num_variables),
-            );
-        }
+        let stream: BenchEvaluationStream<BN254Field> = BenchEvaluationStream::new(num_variables);
+        let prover = TimeProver::<BN254Field>::new(Box::new(&stream));
+        run_bench(
+            c,
+            prover,
+            String::from("vsbw-bn254") + &format!("-{}", num_variables),
+        );
+    }
+}
+
+fn cty_benches(c: &mut Criterion) {
+    let max = 22;
+    // 64 bit field
+    for num_variables in 15..=max {
+        let stream: BenchEvaluationStream<Field64> =
+            BenchEvaluationStream::new(num_variables);
+        let prover = SpaceProver::<Field64>::new(Box::new(&stream));
+        run_bench(
+            c,
+            prover,
+            String::from("cty-fp64") + &format!("-{}", num_variables),
+        );
+    }
+    // 128 bit field
+    for num_variables in 15..=max {
+        let stream: BenchEvaluationStream<Field128> = BenchEvaluationStream::new(num_variables);
+        let prover = SpaceProver::<Field128>::new(Box::new(&stream));
+        run_bench(
+            c,
+            prover,
+            String::from("cty-fp128") + &format!("-{}", num_variables),
+        );
+    }
+    // bn254
+    for num_variables in 15..=max {
+        let stream: BenchEvaluationStream<Field64> = BenchEvaluationStream::new(num_variables);
+        let prover = SpaceProver::<Field64>::new(Box::new(&stream));
+        run_bench(
+            c,
+            prover,
+            String::from("cty-bn254") + &format!("-{}", num_variables),
+        );
+    }
+}
+
+fn tradeoff_k2_benches(c: &mut Criterion) {
+    let max = 22;
+    // 64 bit field
+    for num_variables in 15..=max {
+        let stream: BenchEvaluationStream<Field64> =
+            BenchEvaluationStream::new(num_variables);
+        let prover = TradeoffProver::<Field64>::new(Box::new(&stream), 2);
+        run_bench(
+            c,
+            prover,
+            String::from("tradeoffk2-fp64") + &format!("-{}", num_variables),
+        );
+    }
+    // 128 bit field
+    for num_variables in 15..=max {
+        let stream: BenchEvaluationStream<Field128> = BenchEvaluationStream::new(num_variables);
+        let prover = TradeoffProver::<Field128>::new(Box::new(&stream), 2);
+        run_bench(
+            c,
+            prover,
+            String::from("tradeoffk2-fp128") + &format!("-{}", num_variables),
+        );
+    }
+    // bn254
+    for num_variables in 15..=max {
+        let stream: BenchEvaluationStream<Field64> = BenchEvaluationStream::new(num_variables);
+        let prover = TradeoffProver::<Field64>::new(Box::new(&stream), 2);
+        run_bench(
+            c,
+            prover,
+            String::from("tradeoffk2-bn254") + &format!("-{}", num_variables),
+        );
     }
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = all_benches
+    targets = vsbw_benches, cty_benches
 }
 criterion_main!(benches);
