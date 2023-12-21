9903919aaf89693cc8d881d451f3d5dfe9d76016
Andrew Z <zitek936@gmail.com>
2023-12-21 22:25:16 +0100

    bench tweaks 2

--- benches/stream.rs
+++ benches/stream.rs
@@ -53,9 +53,9 @@ fn run_bench<F: Field, P: Prover<F>>(
 }
 
 fn vsbw_benches(c: &mut Criterion) {
-    let max = 32;
+    let max = 30;
     // 64 bit field
-    for num_variables in 32..=max {
+    for num_variables in 30..=max {
         let stream: BenchEvaluationStream<Field64> = BenchEvaluationStream::new(num_variables);
         let prover = TimeProver::<Field64>::new(Box::new(&stream));
         run_bench(
@@ -66,7 +66,7 @@ fn vsbw_benches(c: &mut Criterion) {
         );
     }
     // 128 bit field
-    for num_variables in 32..=max {
+    for num_variables in 30..=max {
         let stream: BenchEvaluationStream<Field128> = BenchEvaluationStream::new(num_variables);
         let prover = TimeProver::<Field128>::new(Box::new(&stream));
         run_bench(
@@ -77,7 +77,7 @@ fn vsbw_benches(c: &mut Criterion) {
         );
     }
     // bn254
-    for num_variables in 32..=max {
+    for num_variables in 30..=max {
         let stream: BenchEvaluationStream<BN254Field> = BenchEvaluationStream::new(num_variables);
         let prover = TimeProver::<BN254Field>::new(Box::new(&stream));
         run_bench(
@@ -127,7 +127,7 @@ fn cty_benches(c: &mut Criterion) {
 }
 
 fn tradeoff_k2_benches(c: &mut Criterion) {
-    let max = 32;
+    let max = 30;
     // 64 bit field
     for num_variables in 15..=max {
         if num_variables % 2 == 0 {
@@ -170,7 +170,7 @@ fn tradeoff_k2_benches(c: &mut Criterion) {
 }
 
 fn tradeoff_k3_benches(c: &mut Criterion) {
-    let max = 32;
+    let max = 30;
     // 64 bit field
     for num_variables in 15..=max {
         if num_variables % 3 == 0 {
@@ -213,7 +213,7 @@ fn tradeoff_k3_benches(c: &mut Criterion) {
 }
 
 fn tradeoff_k4_benches(c: &mut Criterion) {
-    let max = 32;
+    let max = 30;
     // 64 bit field
     for num_variables in 15..=max {
         if num_variables % 4 == 0 {
@@ -258,6 +258,6 @@ fn tradeoff_k4_benches(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = tradeoff_k2_benches, tradeoff_k3_benches, tradeoff_k4_benches, vsbw_benches
+    targets = tradeoff_k3_benches, tradeoff_k4_benches
 }
 criterion_main!(benches);
