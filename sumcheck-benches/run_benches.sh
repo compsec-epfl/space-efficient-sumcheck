83dd0fb76062ccaa940851ac1f916421355cf166
Andrew Zitek-Estrada <zitek936@gmail.com>
2024-02-22 11:57:49 +0100

    z-tech/rn-blended-to-blendy (#49)

--- sumcheck-benches/run_benches.sh
+++ sumcheck-benches/run_benches.sh
@@ -8,7 +8,7 @@
 # Then:
 #   kill <pid>
 
-algorithms="Blended1 Blended2 VSBW Blended3 Blended4 CTY"
+algorithms="Blendy1 Blendy2 VSBW Blendy3 Blendy4 CTY"
 fields="Field64 Field128 FieldBn254"
 
 for algorithm in $algorithms; do
@@ -16,25 +16,25 @@ for algorithm in $algorithms; do
         num_vars=15
         while [ $num_vars -le 30 ]; do
             case "$algorithm" in
-                "Blended1") stage_size="1" ;;
-                "Blended2") stage_size="2" ;;
-                "Blended3") stage_size="3" ;;
-                "Blended4") stage_size="4" ;;
+                "Blendy1") stage_size="1" ;;
+                "Blendy2") stage_size="2" ;;
+                "Blendy3") stage_size="3" ;;
+                "Blendy4") stage_size="4" ;;
                 "VSBW") stage_size="1" ;;
                 "CTY") stage_size="1" ;;
                 *) ;;
             esac
             case "$algorithm" in
-                "Blended1") algorithm_label="Blended" ;;
-                "Blended2") algorithm_label="Blended" ;;
-                "Blended3") algorithm_label="Blended" ;;
-                "Blended4") algorithm_label="Blended" ;;
+                "Blendy1") algorithm_label="Blendy" ;;
+                "Blendy2") algorithm_label="Blendy" ;;
+                "Blendy3") algorithm_label="Blendy" ;;
+                "Blendy4") algorithm_label="Blendy" ;;
                 "VSBW") algorithm_label="VSBW" ;;
                 "CTY") algorithm_label="CTY" ;;
                 *) ;;
             esac
             # NOTE: if you're on mac you might install gnu-time and change next line to "gtime"
-            output=`(time -v ./target/release/benches $algorithm_label $field $num_vars $stage_size) 2>&1`
+            output=`(gtime -v ./target/release/benches $algorithm_label $field $num_vars $stage_size) 2>&1`
             user_time_seconds=$(echo "$output" | grep "User time (seconds):" | awk '{print $4}')
             user_time_ms=$(awk "BEGIN {printf \"%.0f\", $user_time_seconds * 1000}")
             ram_kilobytes=$(echo "$output" | grep "Maximum resident set size (kbytes)" | awk '{print $6}')
