e28d27f046611a8183048ae02c7cba55ca681fff
Andrew Z <zitek936@gmail.com>
2024-09-05 13:31:16 +0200

    z-tech: cleanup

--- src/provers/lagrange_polynomial.rs
+++ src/provers/lagrange_polynomial.rs
@@ -5,40 +5,14 @@ use super::verifier_messages::VerifierMessages;
 
 #[derive(Debug)]
 pub struct LagrangePolynomial<F: Field> {
-    verifier_messages: VerifierMessages<F>,
-    stop_position: usize,
     last_position: usize,
     position: usize,
     value: F,
+    verifier_messages: VerifierMessages<F>,
+    stop_position: usize,
 }
 
 impl<F: Field> LagrangePolynomial<F> {
-    pub fn new(verifier_messages_original: VerifierMessages<F>) -> Self {
-        let mut verifier_messages = verifier_messages_original.clone();
-        // Iterate over the message_hats, update the running product, and push it onto the stack
-        let mut value: F = F::ONE;
-        for message_hat in &verifier_messages.message_hats_partition_1 {
-            value *= message_hat;
-        }
-
-        // TODO: forgot why these have to be reversed
-        verifier_messages.messages.reverse();
-        verifier_messages.message_hats.reverse();
-
-        verifier_messages.messages_partition_1.reverse();
-        verifier_messages.message_inverses_partition_1.reverse();
-        verifier_messages.message_hats_partition_1.reverse();
-        verifier_messages.message_hat_inverses_partition_1.reverse();
-
-        // Return
-        Self {
-            verifier_messages,
-            value,
-            stop_position: Hypercube::stop_value(verifier_messages_original.messages.len()),
-            position: 0,
-            last_position: 0,
-        }
-    }
     pub fn lag_poly(x: Vec<F>, x_hat: Vec<F>, b: HypercubeMember) -> F {
         // Iterate over the zipped triple x, x_hat, and boolean hypercube vectors
         x.iter().zip(x_hat.iter()).zip(b).fold(
@@ -54,19 +28,28 @@ impl<F: Field> LagrangePolynomial<F> {
             },
         )
     }
+    pub fn new(verifier_messages: VerifierMessages<F>) -> Self {
+        let num_vars = verifier_messages.messages.len();
+        Self {
+            last_position: 0,
+            position: 0,
+            value: verifier_messages.product_of_message_hats,
+            verifier_messages,
+            stop_position: Hypercube::stop_value(num_vars),
+        }
+    }
 }
 
 impl<F: Field> Iterator for LagrangePolynomial<F> {
     type Item = F;
     fn next(&mut self) -> Option<Self::Item> {
-        println!("### position: {}", self.position);
         // Step 1: check if finished iterating
         if self.position >= self.stop_position {
             return None;
         }
 
         // Step 2: check if this iteration yields zero, in which case we skip processing
-        let s: Vec<bool> = self.verifier_messages.messages_partition_2.clone();
+        let s: Vec<bool> = self.verifier_messages.messages_zeros_and_ones.clone();
         let b: Vec<bool> = HypercubeMember::elements_at_indices(
             HypercubeMember::new(self.verifier_messages.messages.len(), self.position)
                 .to_vec_bool(),
@@ -75,49 +58,45 @@ impl<F: Field> Iterator for LagrangePolynomial<F> {
         if s != b {
             // NOTICE! we do not update last_position in this case
             self.position = Hypercube::next_gray_code(self.position);
-            println!("returning zero");
             return Some(F::ZERO);
         }
 
         // Step 3: check if position is 0, which is a special case
-        // Notice! step 2 check could apply to position = 0
+        // Notice! step 2 could apply when position == 0
         if self.position == 0 {
             self.position = Hypercube::next_gray_code(self.position);
             return Some(self.value);
         }
 
-        // Step 4: compute which bit_index flipped and in which direction, we can skip if more than one bit difference
+        // Step 4: update the value, skip if more than one bit difference
         let bit_diff = self.last_position ^ self.position;
         if bit_diff.count_ones() == 1 {
             let index_of_flipped_bit = bit_diff.trailing_zeros() as usize;
             let is_flipped_to_true = self.position & bit_diff != 0;
-            // Step 5: update the value
-            let (divisor, multiplicand) = match is_flipped_to_true {
-                true => (
-                    if self.verifier_messages.message_hats[index_of_flipped_bit] == F::ZERO {
-                        F::ONE
-                    } else {
-                        self.verifier_messages.message_hats[index_of_flipped_bit]
-                    },
-                    if self.verifier_messages.messages[index_of_flipped_bit] == F::ZERO {
-                        F::ONE
-                    } else {
-                        self.verifier_messages.messages[index_of_flipped_bit]
-                    },
-                ),
-                false => (
-                    self.verifier_messages.messages[index_of_flipped_bit],
-                    self.verifier_messages.message_hats[index_of_flipped_bit],
-                ),
-            };
-            self.value = self.value / divisor * multiplicand;
+            let len = self
+                .verifier_messages
+                .product_of_message_and_message_hat_inverses
+                .len();
+            self.value = self.value
+                * match is_flipped_to_true {
+                    true => {
+                        self.verifier_messages
+                            .product_of_message_and_message_hat_inverses
+                            [len - index_of_flipped_bit - 1]
+                    }
+                    false => {
+                        self.verifier_messages
+                            .product_of_message_hat_and_message_inverses
+                            [len - index_of_flipped_bit - 1]
+                    }
+                };
         }
 
-        // Step 6: increment positions
+        // Step 5: increment positions
         self.last_position = self.position;
         self.position = Hypercube::next_gray_code(self.position);
 
-        // Step 7: return
+        // Step 6: return
         Some(self.value)
     }
 }
@@ -131,7 +110,7 @@ mod tests {
 
     #[test]
     fn next() {
-        // remember this is gray code ordering
+        // remember this is gray code ordering!
         let messages: Vec<TestField> =
             vec![TestField::from(13), TestField::from(0), TestField::from(7)];
         let message_hats: Vec<TestField> = messages
@@ -156,8 +135,8 @@ mod tests {
         assert_eq!(lag_poly.next(), None);
     }
     #[test]
-    fn next_boolean() {
-        // remember this is gray code ordering
+    fn boolean_next() {
+        // remember this is gray code ordering!
         let messages: Vec<TestField> =
             vec![TestField::from(0), TestField::from(1), TestField::from(1)];
         let message_hats: Vec<TestField> = messages
--- src/provers/verifier_messages.rs
+++ src/provers/verifier_messages.rs
@@ -2,33 +2,25 @@ use ark_ff::Field;
 
 #[derive(Clone, Debug)]
 pub struct VerifierMessages<F: Field> {
-    // messages and hats
     pub messages: Vec<F>,
-    pub message_hats: Vec<F>,
-    // partitions of all (eight additional)
+    pub message_hats: Vec<F>, // TODO (z-tech): can probably do this differently in blendy
+    pub product_of_message_hats: F,
+    pub product_of_message_and_message_hat_inverses: Vec<F>,
+    pub product_of_message_hat_and_message_inverses: Vec<F>,
     pub indices_of_zero_and_ones: Vec<usize>,
-    pub messages_partition_2: Vec<bool>,
-    // non {0, 1} values
-    pub messages_partition_1: Vec<F>,
-    pub message_hats_partition_1: Vec<F>,
-    pub message_inverses_partition_1: Vec<F>,
-    pub message_hat_inverses_partition_1: Vec<F>,
+    pub messages_zeros_and_ones: Vec<bool>,
 }
 
 impl<F: Field> VerifierMessages<F> {
     pub fn new(messages: &Vec<F>) -> Self {
         let mut verifier_messages = Self {
-            // messages and hats
             messages: vec![],
             message_hats: vec![],
-            // partitions
+            product_of_message_hats: F::ONE,
+            product_of_message_and_message_hat_inverses: vec![],
+            product_of_message_hat_and_message_inverses: vec![],
             indices_of_zero_and_ones: vec![],
-            messages_partition_2: vec![],
-            // non {0, 1} values
-            messages_partition_1: vec![],
-            message_hats_partition_1: vec![],
-            message_inverses_partition_1: vec![],
-            message_hat_inverses_partition_1: vec![],
+            messages_zeros_and_ones: vec![],
         };
         for message in messages {
             verifier_messages.receive_message(*message);
@@ -36,22 +28,29 @@ impl<F: Field> VerifierMessages<F> {
         verifier_messages
     }
     pub fn receive_message(&mut self, message: F) {
-        // calculate
         let message_hat = F::ONE - message;
-        // store
+        let message_inverse = if message == F::ZERO {
+            F::ONE
+        } else {
+            message.inverse().unwrap()
+        };
+        let message_hat_inverse = if message_hat == F::ZERO {
+            F::ONE
+        } else {
+            message_hat.inverse().unwrap()
+        };
         self.messages.push(message);
         self.message_hats.push(message_hat);
-        // partition
+        self.product_of_message_and_message_hat_inverses
+            .push(message * message_hat_inverse);
+        self.product_of_message_hat_and_message_inverses
+            .push(message_hat * message_inverse);
         if message == F::ZERO || message_hat == F::ZERO {
             self.indices_of_zero_and_ones.push(self.messages.len() - 1);
-            self.messages_partition_2
+            self.messages_zeros_and_ones
                 .push(if message == F::ONE { true } else { false });
         } else {
-            self.messages_partition_1.push(message);
-            self.message_hats_partition_1.push(message_hat);
-            self.message_inverses_partition_1.push(F::one() / message);
-            self.message_hat_inverses_partition_1
-                .push(F::one() / message_hat);
+            self.product_of_message_hats = self.product_of_message_hats * message_hat;
         }
     }
 }
@@ -62,108 +61,50 @@ mod tests {
     use ark_ff::{One, Zero};
 
     #[test]
-    fn receive_message_test_1() {
-        let mut m = VerifierMessages::new(&vec![]);
+    fn receive_message() {
+        let mut m0 = VerifierMessages::new(&vec![]);
 
         // ## receive 13
-        m.receive_message(TestField::from(13));
-        // always updated
-        assert_eq!(m.messages, vec![TestField::from(13)]);
-        assert_eq!(m.message_hats, vec![TestField::one() - TestField::from(13)]);
-        // updated when m in {0,1}
-        assert_eq!(m.indices_of_zero_and_ones, vec![]);
-        assert_eq!(m.messages_partition_2, vec![]);
-        // otherwise these updated
-        assert_eq!(m.messages_partition_1, vec![TestField::from(13)]);
+        m0.receive_message(TestField::from(13));
+        assert_eq!(m0.messages, vec![TestField::from(13)]);
         assert_eq!(
-            m.message_hats_partition_1,
+            m0.message_hats,
             vec![TestField::one() - TestField::from(13)]
         );
-        assert_eq!(
-            m.message_inverses_partition_1,
-            vec![TestField::one() / TestField::from(13)]
-        );
-        assert_eq!(
-            m.message_hat_inverses_partition_1,
-            vec![TestField::one() / (TestField::one() - TestField::from(13))]
-        );
+        assert_eq!(m0.indices_of_zero_and_ones, vec![]);
+        assert_eq!(m0.messages_zeros_and_ones, vec![]);
 
         // ## receive 0
-        m.receive_message(TestField::zero());
-        // always updated
-        assert_eq!(m.messages, vec![TestField::from(13), TestField::zero()]);
+        m0.receive_message(TestField::zero());
+        assert_eq!(m0.messages, vec![TestField::from(13), TestField::zero()]);
         assert_eq!(
-            m.message_hats,
+            m0.message_hats,
             vec![TestField::one() - TestField::from(13), TestField::one()]
         );
-        // updated when m in {0,1}
-        assert_eq!(m.indices_of_zero_and_ones, vec![1]);
-        assert_eq!(m.messages_partition_2, vec![false]);
-        // otherwise these updated
-        assert_eq!(m.messages_partition_1, vec![TestField::from(13)]);
-        assert_eq!(
-            m.message_hats_partition_1,
-            vec![TestField::one() - TestField::from(13)]
-        );
-        assert_eq!(
-            m.message_inverses_partition_1,
-            vec![TestField::one() / TestField::from(13)]
-        );
-        assert_eq!(
-            m.message_hat_inverses_partition_1,
-            vec![TestField::one() / (TestField::one() - TestField::from(13))]
-        );
+        assert_eq!(m0.indices_of_zero_and_ones, vec![1]);
+        assert_eq!(m0.messages_zeros_and_ones, vec![false]);
 
         // ## receive 7
-        m.receive_message(TestField::from(7));
-        // always updated
+        m0.receive_message(TestField::from(7));
         assert_eq!(
-            m.messages,
+            m0.messages,
             vec![TestField::from(13), TestField::zero(), TestField::from(7)]
         );
         assert_eq!(
-            m.message_hats,
+            m0.message_hats,
             vec![
                 TestField::one() - TestField::from(13),
                 TestField::one(),
                 TestField::one() - TestField::from(7)
             ]
         );
-        // updated when m in {0,1}
-        assert_eq!(m.indices_of_zero_and_ones, vec![1]);
-        assert_eq!(m.messages_partition_2, vec![false]);
-        // otherwise these updated
-        assert_eq!(
-            m.messages_partition_1,
-            vec![TestField::from(13), TestField::from(7)]
-        );
-        assert_eq!(
-            m.message_hats_partition_1,
-            vec![
-                TestField::one() - TestField::from(13),
-                TestField::one() - TestField::from(7)
-            ]
-        );
-        assert_eq!(
-            m.message_inverses_partition_1,
-            vec![
-                TestField::one() / TestField::from(13),
-                TestField::one() / TestField::from(7)
-            ]
-        );
-        assert_eq!(
-            m.message_hat_inverses_partition_1,
-            vec![
-                TestField::one() / (TestField::one() - TestField::from(13)),
-                TestField::one() / (TestField::one() - TestField::from(7))
-            ]
-        );
+        assert_eq!(m0.indices_of_zero_and_ones, vec![1]);
+        assert_eq!(m0.messages_zeros_and_ones, vec![false]);
 
         // ## receive 1
-        m.receive_message(TestField::one());
-        // always updated
+        m0.receive_message(TestField::one());
         assert_eq!(
-            m.messages,
+            m0.messages,
             vec![
                 TestField::from(13),
                 TestField::zero(),
@@ -172,7 +113,7 @@ mod tests {
             ]
         );
         assert_eq!(
-            m.message_hats,
+            m0.message_hats,
             vec![
                 TestField::one() - TestField::from(13),
                 TestField::one(),
@@ -180,67 +121,23 @@ mod tests {
                 TestField::zero()
             ]
         );
-        // updated when m in {0,1}
-        assert_eq!(m.indices_of_zero_and_ones, vec![1, 3]);
-        assert_eq!(m.messages_partition_2, vec![false, true]);
-        // otherwise these updated
-        assert_eq!(
-            m.messages_partition_1,
-            vec![TestField::from(13), TestField::from(7)]
-        );
-        assert_eq!(
-            m.message_hats_partition_1,
-            vec![
-                TestField::one() - TestField::from(13),
-                TestField::one() - TestField::from(7)
-            ]
-        );
-        assert_eq!(
-            m.message_inverses_partition_1,
-            vec![
-                TestField::one() / TestField::from(13),
-                TestField::one() / TestField::from(7)
-            ]
-        );
-        assert_eq!(
-            m.message_hat_inverses_partition_1,
-            vec![
-                TestField::one() / (TestField::one() - TestField::from(13)),
-                TestField::one() / (TestField::one() - TestField::from(7))
-            ]
-        );
-    }
+        assert_eq!(m0.indices_of_zero_and_ones, vec![1, 3]);
+        assert_eq!(m0.messages_zeros_and_ones, vec![false, true]);
 
-    #[test]
-    fn receive_message_test_2() {
-        let mut m = VerifierMessages::new(&vec![]);
+        let mut m1 = VerifierMessages::new(&vec![]);
 
         // ## receive zero
-        m.receive_message(TestField::from(0));
-        // always updated
-        assert_eq!(m.messages, vec![TestField::from(0)]);
-        assert_eq!(m.message_hats, vec![TestField::one()]);
-        // updated when m in {0,1}
-        assert_eq!(m.indices_of_zero_and_ones, vec![0]);
-        assert_eq!(m.messages_partition_2, vec![false]);
-        // otherwise these updated
-        assert_eq!(m.messages_partition_1, vec![]);
-        assert_eq!(m.message_hats_partition_1, vec![]);
-        assert_eq!(m.message_inverses_partition_1, vec![]);
-        assert_eq!(m.message_hat_inverses_partition_1, vec![]);
+        m1.receive_message(TestField::from(0));
+        assert_eq!(m1.messages, vec![TestField::from(0)]);
+        assert_eq!(m1.message_hats, vec![TestField::one()]);
+        assert_eq!(m1.indices_of_zero_and_ones, vec![0]);
+        assert_eq!(m1.messages_zeros_and_ones, vec![false]);
 
         // receive 1
-        m.receive_message(TestField::from(1));
-        // always updated
-        assert_eq!(m.messages, vec![TestField::from(0), TestField::one()]);
-        assert_eq!(m.message_hats, vec![TestField::one(), TestField::zero()]);
-        // updated when m in {0,1}
-        assert_eq!(m.indices_of_zero_and_ones, vec![0, 1]);
-        assert_eq!(m.messages_partition_2, vec![false, true]);
-        // otherwise these updated
-        assert_eq!(m.messages_partition_1, vec![]);
-        assert_eq!(m.message_hats_partition_1, vec![]);
-        assert_eq!(m.message_inverses_partition_1, vec![]);
-        assert_eq!(m.message_hat_inverses_partition_1, vec![]);
+        m1.receive_message(TestField::from(1));
+        assert_eq!(m1.messages, vec![TestField::from(0), TestField::one()]);
+        assert_eq!(m1.message_hats, vec![TestField::one(), TestField::zero()]);
+        assert_eq!(m1.indices_of_zero_and_ones, vec![0, 1]);
+        assert_eq!(m1.messages_zeros_and_ones, vec![false, true]);
     }
 }
