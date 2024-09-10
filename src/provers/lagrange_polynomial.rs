a18c413eca95257f3300093057c9445ac8ea909f
Andrew Zitek-Estrada <zitek936@gmail.com>
2024-09-10 17:01:38 +0200

    z-tech: lag poly benches and performance tweaks (#56)
    * fix
    
    * fix to_evals ordering
    
    * cleanup
    
    * fix gray code range initialization
    
    * z-tech: clean up, rm dep
    
    * gray code seq lag poly
    
    * z-tech: Blendy uses gray code
    
    * z-tech: clean and rename
    
    * z-tech: batch inversion
    
    * z-tech: limit struct visibility
    
    * partitioned lag poly chkpt
    
    * z-tech: verifier messages struct chkpt
    
    * generalized seq lag poly chkpt
    
    * chkpt
    
    * chkpt
    
    * z-tech: generalized seq lag poly
    
    * z-tech: fix generalized seq lag poly, integrate with blendy for boolean inputs
    
    * z-tech: cleanup
    
    * z-tech: more cleanup
    
    * ds_store
    
    * gitignore ds_store
    
    * benches
    
    * lint
    
    * z-tech: lag poly generalizes to zero one
    
    * bitwise check instead of vec bool
    
    * fmt

--- src/provers/lagrange_polynomial.rs
+++ src/provers/lagrange_polynomial.rs
@@ -49,13 +49,9 @@ impl<F: Field> Iterator for LagrangePolynomial<F> {
         }
 
         // Step 2: check if this iteration yields zero, in which case we skip processing
-        let s: Vec<bool> = self.verifier_messages.messages_zeros_and_ones.clone();
-        let b: Vec<bool> = HypercubeMember::elements_at_indices(
-            HypercubeMember::new(self.verifier_messages.messages.len(), self.position)
-                .to_vec_bool(),
-            self.verifier_messages.indices_of_zero_and_ones.clone(),
-        );
-        if s != b {
+        let all_agreement = !(self.verifier_messages.messages_zeros_and_ones_usize ^ self.position);
+        let zero_one_agreement = all_agreement & self.verifier_messages.mask;
+        if zero_one_agreement != self.verifier_messages.mask {
             // NOTICE! we do not update last_position in this case
             self.position = Hypercube::next_gray_code(self.position);
             return Some(F::ZERO);
