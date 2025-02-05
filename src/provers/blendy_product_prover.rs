412ce5b0b9637127fc74f8a649583ac8506aba5d
Andrew Z <{ID}+{username}@users.noreply.github.com>
2025-02-05 11:02:02 +0100

    checkpoint

---
+++ src/provers/blendy_product_prover.rs
@@ -0,0 +1,309 @@
+use std::marker::PhantomData;
+
+use ark_ff::Field;
+use ark_std::vec::Vec;
+
+use crate::provers::{
+    evaluation_stream::EvaluationStream,
+    hypercube::Hypercube,
+    lagrange_polynomial::LagrangePolynomial,
+    prover::{Prover, ProverArgs, ProverArgsStageInfo},
+};
+
+use super::verifier_messages::VerifierMessages;
+
+pub struct BlendyProductProver<'a, F: Field, S: EvaluationStream<F>> {
+    claimed_sum: F,
+    current_round: usize,
+    evaluation_stream_1: &'a S,
+    evaluation_stream_2: &'a S,
+    num_stages: usize,
+    num_variables: usize,
+    verifier_messages: VerifierMessages<F>,
+    x_table: Vec<F>,
+    y_table: Vec<F>,
+    j_prime_table: Vec<Vec<F>>,
+    stage_size: usize,
+}
+
+impl<'a, F: Field, S: EvaluationStream<F>> BlendyProductProver<'a, F, S> {
+    const DEFAULT_NUM_STAGES: usize = 2;
+
+    fn compute_round(&self) -> (F, F) {
+        // Initialize accumulators for sum_0 and sum_1
+        let mut sum_0 = F::ZERO;
+        let mut sum_1 = F::ZERO;
+        let mut sum_half = F::ZERO;
+
+        let j_prime: usize;
+        let t: usize;
+        println!("num_variables: {:?}", self.num_variables);
+        let l = self.num_variables.div_ceil(2 * self.num_stages);
+        println!("l: {:?}", l);
+        let j = self.current_round + 1;
+        println!("j: {:?}", j);
+        let mut p = 0_usize;
+        let s = (j).ilog2() as usize;
+        println!("s: {:?}", s);
+        println!("l - 1: {:?}", l - 1);
+        if j <= l - 1 {
+            if 2_usize.pow(s as u32) == j {
+                p = 1;
+            }
+            j_prime = 2_usize.pow(s as u32);
+            t = j_prime - 1;
+        } else {
+            if j % l == 0 {
+                p = 1;
+            }
+            j_prime = l * (j / l);
+            t = l - 1;
+        }
+        println!("j_prime: {:?}", j_prime);
+        println!("t: {:?}", t);
+
+
+        let r1: Vec<F> = self.verifier_messages.messages[0..j_prime-1].to_vec();
+        let mut r1_hat: Vec<F> = vec![F::ZERO; r1.len()];
+        for i in 0..r1.len() {
+            r1_hat[i] = F::ONE - r1[i];
+        }
+        let r2: Vec<F> = self.verifier_messages.messages[j_prime-1..].to_vec();
+        let mut r2_hat: Vec<F> = vec![F::ZERO; r2.len()];
+        for i in 0..r2.len() {
+            r2_hat[i] = F::ONE - r2[i];
+        }
+
+        for (b_prime_index, b_prime) in Hypercube::new(j - j_prime) {
+            let lag_poly_1 =
+            LagrangePolynomial::lag_poly(r2.clone(), r2_hat.clone(), b_prime.clone());
+            for (b_prime_prime_index, b_prime_prime) in Hypercube::new(j - j_prime) {
+                let lag_poly_2 =
+                    LagrangePolynomial::lag_poly(r2.clone(), r2_hat.clone(), b_prime_prime.clone());
+                println!("b_prime_prime: {:?}", b_prime_prime);
+                println!("r1: {:?}", r1);
+                println!("r2: {:?}", r2);
+                println!("lag_poly_1: {:?}", lag_poly_1);
+                println!("lag_poly_2: {:?}", lag_poly_2);
+                for (v_index, _) in Hypercube::new((t as i64 - j as i64 + j_prime as i64) as usize) {
+                    let b_prime_0_v = v_index << (j - j_prime + 1)| 0 << (j - j_prime) | b_prime_index;
+                    let b_prime_prime_0_v = v_index << (j - j_prime+ 1)| 0 << (j - j_prime) | b_prime_prime_index;
+                    let b_prime_1_v = v_index << (j - j_prime + 1)| 1 << (j - j_prime) | b_prime_index;
+                    let b_prime_prime_1_v = v_index << (j - j_prime+ 1)| 1 << (j - j_prime) | b_prime_prime_index;
+                    println!("b_prime_0_v {:?}", b_prime_0_v);
+                    println!("b_prime_prime_0_v {:?}", b_prime_0_v);
+                    println!("adding: {:?}", self.j_prime_table[b_prime_0_v][b_prime_prime_0_v]);
+                    sum_0 += lag_poly_1 * lag_poly_2 * self.j_prime_table[b_prime_0_v][b_prime_prime_0_v];
+                    sum_1 += self.j_prime_table[b_prime_1_v][b_prime_prime_1_v];
+                    sum_half += self.j_prime_table[b_prime_0_v][b_prime_prime_0_v];
+                    sum_half += self.j_prime_table[b_prime_0_v][b_prime_prime_1_v];
+                    sum_half += self.j_prime_table[b_prime_1_v][b_prime_prime_0_v];
+                    sum_half += self.j_prime_table[b_prime_1_v][b_prime_prime_1_v];
+                }
+                // sum_0 = sum_0 * lag_poly_1 * lag_poly_2;
+                sum_1 = sum_1 * lag_poly_1 * lag_poly_2;
+                sum_half = sum_half * lag_poly_1 * lag_poly_2;
+            }
+        }
+        sum_half = sum_half / F::from(4_u32);
+
+        // Return the accumulated sums
+        (sum_0, sum_1)
+    }
+
+    fn is_initial_round(&self) -> bool {
+        self.current_round == 0
+    }
+
+    fn compute_state(&mut self) {
+        println!("####### compute state");
+        let j_prime: usize;
+        let t: usize;
+        println!("num_variables: {:?}", self.num_variables);
+        let l = self.num_variables.div_ceil(2 * self.num_stages);
+        println!("l: {:?}", l);
+        let j = self.current_round + 1;
+        println!("j: {:?}", j);
+        let mut p = 0_usize;
+        let s = (j).ilog2() as usize;
+        println!("s: {:?}", s);
+        println!("l - 1: {:?}", l - 1);
+        if j <= l - 1 {
+            if 2_usize.pow(s as u32) == j {
+                p = 1;
+            }
+            j_prime = 2_usize.pow(s as u32);
+            t = j_prime - 1;
+        } else {
+            if j % l == 0 {
+                p = 1;
+            }
+            j_prime = l * (j / l);
+            t = l - 1;
+        }
+        println!("j_prime: {:?}", j_prime);
+        println!("t: {:?}", t);
+
+        if p == 1 {
+            self.j_prime_table = vec![
+                vec![F::ZERO; Hypercube::stop_value(self.stage_size)];
+                Hypercube::stop_value(self.stage_size)
+            ];
+            let b_num_vars = self.num_variables - j_prime - t;
+            for (b_index, _) in Hypercube::new(b_num_vars) {
+
+                println!("####### X update");
+
+                for (b_prime_index, _) in Hypercube::new(t + 1) {
+                    self.x_table[b_prime_index] = F::ZERO;
+                    for (x_index, x) in Hypercube::new(j_prime - 1) {
+                        let evaluation_point = x_index << (t + 1 + b_num_vars) | b_prime_index << b_num_vars | b_index;
+                        println!("point: {:?}", evaluation_point);
+                        let lag_poly = LagrangePolynomial::lag_poly(
+                            self.verifier_messages.messages[0..j_prime-1].to_vec(),
+                            self.verifier_messages.message_hats[0..j_prime-1].to_vec(),
+                            x,
+                        );
+                        println!("lag_poly: {:?}, eval: {:?}", lag_poly, self.evaluation_stream_1.get_evaluation(evaluation_point));
+                        self.x_table[b_prime_index] +=
+                        lag_poly * self.evaluation_stream_1.get_evaluation(evaluation_point);
+                        println!("x_table: {:?}", self.x_table);
+                    }
+                }
+
+                println!("####### Y update");
+
+                for (b_prime_prime_index, _) in Hypercube::new(t + 1) {
+                    self.y_table[b_prime_prime_index] = F::ZERO;
+                    for (x_index, x) in Hypercube::new(j_prime - 1) {
+                        let evaluation_point = x_index << (t + 1 + b_num_vars) | b_prime_prime_index << b_num_vars | b_index;
+                        println!("b_prime_prime_index: {:?}", b_prime_prime_index);
+                        println!("point: {:?}", evaluation_point);
+                        let lag_poly = LagrangePolynomial::lag_poly(
+                            self.verifier_messages.messages[0..j_prime-1].to_vec(),
+                            self.verifier_messages.message_hats[0..j_prime-1].to_vec(),
+                            x,
+                        );
+                        println!("lag_poly: {:?}, eval: {:?}", lag_poly, self.evaluation_stream_2.get_evaluation(evaluation_point));
+                        self.y_table[b_prime_prime_index] +=
+                        lag_poly * self.evaluation_stream_2.get_evaluation(evaluation_point);
+                        println!("y_table: {:?}", self.y_table);
+                    }
+                }
+
+                println!("####### M update");
+
+                for (b_prime_index, _) in Hypercube::new(t + 1) {
+                    for (b_prime_prime_index, _) in Hypercube::new(t + 1) {
+                        // println!("adding to j_prime table: {:?}", (self.x_table[b_prime_index] * self.y_table[b_prime_prime_index]));
+                        self.j_prime_table[b_prime_index][b_prime_prime_index] = self.j_prime_table
+                            [b_prime_index][b_prime_prime_index]
+                            + (self.x_table[b_prime_index] * self.y_table[b_prime_prime_index]);
+                    }
+                }
+            }
+
+            println!("########");
+            println!("j_prime_table[0][0]: {:?}", self.j_prime_table[0][0]);
+            println!("j_prime_table[0][1]: {:?}", self.j_prime_table[0][1]);
+            println!("j_prime_table[1][0]: {:?}", self.j_prime_table[1][0]);
+            println!("j_prime_table[1][1]: {:?}", self.j_prime_table[1][1]);
+        }
+    }
+}
+
+impl<'a, F: Field, S: EvaluationStream<F>> Prover<'a, F, S> for BlendyProductProver<'a, F, S> {
+    fn claimed_sum(&self) -> F {
+        self.claimed_sum
+    }
+
+    fn generate_default_args(stream: &'a S) -> ProverArgs<'a, F, S> {
+        ProverArgs {
+            stream,
+            stage_info: Some(ProverArgsStageInfo {
+                num_stages: BlendyProductProver::<F, S>::DEFAULT_NUM_STAGES,
+            }),
+            _phantom: PhantomData,
+        }
+    }
+
+    fn new(prover_args: ProverArgs<'a, F, S>) -> Self {
+        let claimed_sum: F = prover_args.stream.get_claimed_sum();
+        let num_variables: usize = prover_args.stream.get_num_variables();
+        let num_stages: usize = prover_args.stage_info.unwrap().num_stages;
+        let stage_size: usize = num_variables / num_stages;
+        // return the BlendyProver instance
+        Self {
+            claimed_sum,
+            current_round: 0,
+            evaluation_stream_1: prover_args.stream,
+            evaluation_stream_2: prover_args.stream,
+            num_stages,
+            num_variables,
+            verifier_messages: VerifierMessages::new(&vec![]),
+            x_table: vec![F::ZERO; Hypercube::stop_value(num_variables.div_ceil(2 * num_stages))],
+            y_table: vec![F::ZERO; Hypercube::stop_value(num_variables.div_ceil(2 * num_stages))],
+            j_prime_table: vec![
+                vec![F::ZERO; Hypercube::stop_value(stage_size)];
+                Hypercube::stop_value(stage_size)
+            ],
+            stage_size,
+        }
+    }
+
+    fn next_message(&mut self, verifier_message: Option<F>) -> Option<(F, F)> {
+        // Ensure the current round is within bounds
+        if self.current_round >= self.total_rounds() {
+            return None;
+        }
+
+        if !self.is_initial_round() {
+            self.verifier_messages
+                .receive_message(verifier_message.unwrap());
+        }
+
+        self.compute_state();
+
+        let sums: (F, F) = self.compute_round();
+
+        // Increment the round counter
+        self.current_round += 1;
+
+        // Return the computed polynomial sums
+        Some(sums)
+    }
+
+    fn total_rounds(&self) -> usize {
+        self.num_variables
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::marker::PhantomData;
+
+    use crate::provers::{
+        prover::{Prover, ProverArgs, ProverArgsStageInfo},
+        test_helpers::{
+            run_product_sumcheck_test, test_polynomial_2, BasicEvaluationStream, TestField,
+        },
+        BlendyProductProver,
+    };
+
+    #[test]
+    fn sumcheck() {
+        let evaluation_stream: BasicEvaluationStream<TestField> =
+            BasicEvaluationStream::new(test_polynomial_2());
+
+        // k=2 (DEFAULT)
+        run_product_sumcheck_test(BlendyProductProver::new(BlendyProductProver::generate_default_args(
+            &evaluation_stream,
+        )));
+        // k=3
+        run_product_sumcheck_test(BlendyProductProver::new(ProverArgs {
+            stream: &evaluation_stream,
+            stage_info: Some(ProverArgsStageInfo { num_stages: 3 }),
+            _phantom: PhantomData,
+        }));
+    }
+}
--- src/provers/test_helpers.rs
+++ src/provers/test_helpers.rs
@@ -51,7 +51,7 @@ pub fn run_boolean_sumcheck_test<
         F::from(11),
         "g0 should evaluate correctly for input 1"
     );
-    // FIRST ROUND x0 fixed to 0
+    // FIRST ROUND x0 fixed to 1
     // 101 = 2
     // 100 = 2
     // sum g1(0) = 4
@@ -142,6 +142,114 @@ pub fn run_basic_sumcheck_test<
     );
 }
 
+pub fn run_product_sumcheck_test<
+    'a,
+    F: Field + std::convert::From<i32>,
+    S: EvaluationStream<F>,
+    P: Prover<'a, F, S>,
+>(
+    mut prover: P,
+) {
+    // ZEROTH ROUND
+    // 0000 = 0 * 0 = 0
+    // 0001 = 1 * 1 = 1
+    // 0010 = 0 * 0 = 0
+    // 0011 = 1 * 1 = 1
+    // 0100 = 13 * 13 = 17
+    // 0101 = 14 * 14 = 6
+    // 0110 = 1 * 1 = 1
+    // 0111 = 2 * 2 = 4
+    // sum g0(0) = 11
+    // 1000 = 2 * 2 = 4
+    // 1001 = 3 * 3 = 9
+    // 1010 = 2 * 2 = 4
+    // 1011 = 3 * 3 = 9
+    // 1100 = 0 * 0 = 0
+    // 1101 = 1 * 1 = 1
+    // 1110 = 7 * 7 = 11
+    // 1111 = 8 * 8 = 7
+    // sum g0(1) = 7
+    let round_0 = prover.next_message(None).unwrap();
+    assert_eq!(
+        round_0.0,
+        F::from(11),
+        "g0 should evaluate correctly for input 0"
+    );
+    assert_eq!(
+        round_0.1,
+        F::from(7),
+        "g0 should evaluate correctly for input 1"
+    );
+    // FIRST ROUND x0 fixed to 3
+    // 3000 = (0 * 17) + (2 * 3) = 6 * 6 = 17
+    // 3001 = (1 * 17) + (3 * 3) = 7 * 7 = 11
+    // 3010 = (0 * 17) + (2 * 3) = 6 * 6 = 17
+    // 3011 = (1 * 17) + (3 * 3) = 7 * 7 = 11
+    // sum g1(0) = 18
+    // 3100 = (13 * 17) + (0 * 3) = 12 * 12 = 11
+    // 3101 = (14 * 17) + (1 * 3) = 13 * 13 = 17
+    // 3110 = (1 * 17) + (7 * 3) = 0 * 0 = 0
+    // 3111 = (2 * 17) + (8 * 3) = 1 * 1 = 1
+    // sum g1(1) = 10
+    let round_1 = prover.next_message(Some(F::from(3))).unwrap(); // x0 fixed to 3
+    // assert_eq!(
+    //     round_0.0 - (round_0.0 - round_0.1) * F::from(3) * F::from(3),
+    //     round_1.0 + round_1.1
+    // );
+    assert_eq!(
+        round_1.0,
+        F::from(18),
+        "g1 should evaluate correctly for input 0"
+    );
+    assert_eq!(
+        round_1.1,
+        F::from(10),
+        "g1 should evaluate correctly for input 1"
+    );
+    // SECOND ROUND x1 fixed to 4
+    // 3400 = (6 * 16) + (12 * 4) = 11 * 11 = 7
+    // 3401 = (7 * 16) + (13 * 4) = 12 * 12 = 11
+    // sum g2(0) = 18
+    // 3410 = (6 * 16) + (0 * 4) = 1 * 1 = 1
+    // 3411 = (7 * 16) + (1 * 4) = 2 * 2 = 4
+    // sum g2(1) = 5
+    let round_2 = prover.next_message(Some(F::from(4))).unwrap(); // x1 fixed to 4
+    // assert_eq!(
+    //     round_1.0 - (round_1.0 - round_1.1) * F::from(4),
+    //     round_2.0 + round_2.1
+    // );
+    assert_eq!(
+        round_2.0,
+        F::from(18),
+        "g2 should evaluate correctly for input 0"
+    );
+    assert_eq!(
+        round_2.1,
+        F::from(5),
+        "g2 should evaluate correctly for input 1"
+    );
+    // LAST ROUND x2 fixed to 7
+    // 3470 = (11 * 13) + (1 * 7) = 17 * 17 = 4
+    // sum g3(0) = 4
+    // 3471 = (12 * 13) + (2 * 7) = 18 * 18 = 1
+    // sum g3(1) =
+    let round_2 = prover.next_message(Some(F::from(7))).unwrap(); // x2 fixed to 7
+    // assert_eq!(
+    //     round_1.0 - (round_1.0 - round_1.1) * F::from(4),
+    //     round_2.0 + round_2.1
+    // );
+    assert_eq!(
+        round_2.0,
+        F::from(4),
+        "g3 should evaluate correctly for input 0"
+    );
+    assert_eq!(
+        round_2.1,
+        F::from(1),
+        "g3 should evaluate correctly for input 1"
+    );
+}
+
 pub fn test_polynomial() -> Vec<TestField> {
     // 4*x_1*x_2 + 7*x_2*x_3 + 2*x_1 + 13*x_2
     return TestPolynomial::from_coefficients_slice(
@@ -162,6 +270,36 @@ pub fn test_polynomial() -> Vec<TestField> {
             (
                 TestField::from(13),
                 multivariate::SparseTerm::new(vec![(1, 1)]),
+            ),
+        ],
+    )
+    .to_evaluations();
+}
+
+pub fn test_polynomial_2() -> Vec<TestField> {
+    // 4*x_1*x_2 + 7*x_2*x_3 + 2*x_1 + 13*x_2 + 1x_4
+    return TestPolynomial::from_coefficients_slice(
+        4,
+        &[
+            (
+                TestField::from(4),
+                multivariate::SparseTerm::new(vec![(0, 1), (1, 1)]),
+            ),
+            (
+                TestField::from(7),
+                multivariate::SparseTerm::new(vec![(1, 1), (2, 1)]),
+            ),
+            (
+                TestField::from(2),
+                multivariate::SparseTerm::new(vec![(0, 1)]),
+            ),
+            (
+                TestField::from(13),
+                multivariate::SparseTerm::new(vec![(1, 1)]),
+            ),
+            (
+                TestField::from(1),
+                multivariate::SparseTerm::new(vec![(3, 1)]),
             ),
         ],
     )
